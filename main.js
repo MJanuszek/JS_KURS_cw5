a91b14cffc6e1ff9716eff8c15897db38984493e
MJanuszek <monika.januszek.mail@gmail.com>
2022-05-11 13:50:31 +0200

    Img slider operated by keyCode
    keyChangeSlide function added

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const slideList = [
     }, 
     {   img: "gallery/clay-banks-hwLAI5lRhdM-unsplash (1).jpg",
         text: "Nihon-go wa totemo merodikku desu."}, 
-    {   img: "sora-sagano-8sOZJ8JF0S8-unsplash.jpg",
+    {   img: "gallery/sora-sagano-8sOZJ8JF0S8-unsplash.jpg",
         text: "Nihon wa kirei desu."},
 ];
 
@@ -14,7 +14,15 @@ let activImgElement = 0;
 
 const image = document.querySelector("img.slider");
 const h1 = document.querySelector("h1.slider");
+// rest [...] > zmiana elementów html na tablicę:::
+const dots = [...document.querySelectorAll(".dots span")];
 
+const changeDot = () => {
+    // zostanie zwrócony index elementu z klasą active
+    const activeDot = dots.findIndex(dot => dot.classList.contains("active"));
+    dots[activeDot].classList.remove("active");
+    dots[activImgElement].classList.add("active");
+}
 
 const changeSlide = function() {
     activImgElement++;
@@ -24,11 +32,29 @@ const changeSlide = function() {
     image.src = slideList[activImgElement].img;
     h1.textContent = slideList[activImgElement].text;
 
+    changeDot();
 
 }
 
+const keyChangeSlide = function(e) {
+    console.log(e.keyCode);
+    if(e.keyCode == 37 || e.keyCode == 39){
+        clearInterval(indexInterval);
+        e.keyCode == 37? activImgElement-- : activImgElement++;
+        if (activImgElement === slideList.length ){
+            activImgElement = 0;
+        } else if (activImgElement < 0){
+            activImgElement = slideList.length-1;
+        }
 
-
-
+        image.src = slideList[activImgElement].img;
+        h1.textContent = slideList[activImgElement].text;
+        changeDot();
+        indexInterval = setInterval(changeSlide, timeOfSlide);
+    }
+}
 // zmiana slidu, wywołanie funkcji changeSlide
-setInterval(changeSlide, timeOfSlide)
+let indexInterval = setInterval(changeSlide, timeOfSlide);
+
+// zmiana slidu za poomocą strzałek na klawiaturze:
+window.addEventListener("keydown", keyChangeSlide);
